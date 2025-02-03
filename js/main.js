b4b085b6615ee6d0744415982cc7bde5938caa69
kosscoder <91034680+kosscoder@users.noreply.github.com>
2025-02-03 15:03:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const mobileImagesContainer = document.querySelector('.mobile__images');
+
+mobileImagesContainer.addEventListener('click', event => {
+
+	if (event.target.tagName !== 'IMG') return;
+
+	event.target.classList.toggle('image-scale-null');
+})
 
 
 // once - анимация отрабатывает один раз
